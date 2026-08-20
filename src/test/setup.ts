70ea772a36e78a49526db1a3596fcd17fbd5a334
m45801ch <m45801ch@gmail.com>
2026-08-20 10:43:42 +0800

    fix: align node types with vitest

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,11 +1,7 @@
 import { vi } from "vitest";
 
-vi.mock(
-  "obsidian",
-  () => ({
-    Notice: class Notice {},
-    TFile: class TFile {},
-    TFolder: class TFolder {},
-  }),
-  { virtual: true },
-);
+vi.mock("obsidian", () => ({
+  Notice: class Notice {},
+  TFile: class TFile {},
+  TFolder: class TFolder {},
+}));
